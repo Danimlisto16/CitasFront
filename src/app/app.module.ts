843cc6bb8bf4225a2ce91ae3e2ebbd31f9d055b5
Daniel Yanez Bravo <Dayanez8@espe.edu.ec>
2020-09-06 19:56:21 -0500

    Cambios Realizados en pacientes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PacientesListComponent } from './Components/pacientes-list/pacientes-li
 import { CitaMainComponent } from './Components/cita-main/cita-main.component';
 
 import { MedicosListComponent } from './Components/medicos-list/medicos-list.component';
+import { FichaPacienteComponent } from './Components/ficha-paciente/ficha-paciente.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,9 @@ import { MedicosListComponent } from './Components/medicos-list/medicos-list.com
     PacientesListComponent,
     CitaMainComponent,
   
-    MedicosListComponent
+    MedicosListComponent,
+  
+    FichaPacienteComponent
     
   ],
   imports: [
